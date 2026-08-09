9953bc926f5db1960c03c57f1379f9c1c08a8398
Chase Bryan <142706090+chasebryan@users.noreply.github.com>
2026-08-09 05:26:47 -0500

    Broaden deterministic CENTL-SCi class routing

--- src/ocaml/centl_sci_hint.ml
+++ src/ocaml/centl_sci_hint.ml
@@ -1,4 +1,9 @@
-type t = Any | Exact_expression | Polynomial_equation | Unit_conversion
+type t =
+  | Any
+  | Exact_expression
+  | Polynomial_equation
+  | Unit_conversion
+  | Unsupported of string
 
 let contains ~needle text =
   let needle_length = String.length needle in
@@ -11,15 +16,56 @@ let contains ~needle text =
   in
   loop 0
 
+let starts prefix text = String.starts_with ~prefix text
+
 let classify problem =
   let problem = String.trim problem |> String.lowercase_ascii in
-  let solve = String.starts_with ~prefix:"solve " problem in
+  let has_relation =
+    contains ~needle:" equals " problem || contains ~needle:"=" problem
+  in
   let has_variable_target = contains ~needle:" for " problem in
-  let has_relation = contains ~needle:" equals " problem || contains ~needle:"=" problem in
-  if solve && has_variable_target && has_relation then Polynomial_equation else Any
+  let equation_request =
+    has_relation
+    && ((contains ~needle:"solve " problem && has_variable_target)
+       || contains ~needle:" satisfying " problem
+       || contains ~needle:"value of " problem
+       || contains ~needle:"determine the value" problem)
+  in
+  let contradictory =
+    contains ~needle:"but also assume" problem
+    || contains ~needle:"contradict" problem
+  in
+  let mechanics =
+    contains ~needle:"dropped from" problem
+    || contains ~needle:"air resistance" problem
+    || contains ~needle:"before impact" problem
+    || contains ~needle:"spring" problem
+  in
+  let general_knowledge =
+    starts "who " problem || starts "who was " problem || starts "who is " problem
+  in
+  if contradictory then Unsupported "contradictory request"
+  else if mechanics then
+    if contains ~needle:"spring" problem then
+      Unsupported "missing physics parameters or unsupported mechanics"
+    else Unsupported "mechanics outside CENTL-SCi v0.0.1"
+  else if general_knowledge then Unsupported "general knowledge outside CENTL-SCi"
+  else if
+    starts "convert " problem
+    || (starts "how many " problem && Centl_sci_units.mentions_known_unit problem)
+  then Unit_conversion
+  else if equation_request then Polynomial_equation
+  else if
+    starts "what is " problem || starts "calculate " problem
+    || starts "compute " problem || starts "evaluate " problem
+  then Exact_expression
+  else Any
 
 let text = function
   | Any -> "any"
   | Exact_expression -> "exact_expression"
   | Polynomial_equation -> "polynomial_equation"
   | Unit_conversion -> "unit_conversion"
+  | Unsupported _ -> "unsupported"
+
+let reason_hint = function Unsupported reason -> Some reason | _ -> None
