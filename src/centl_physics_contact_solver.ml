e949a525cc6ae9b18f1327dd71806b7a5f4e6d83
Chase Bryan <142706090+chasebryan@users.noreply.github.com>
2026-08-08 22:38:06 -0500

    Fix contact composition state naming

--- src/centl_physics_contact_solver.ml
+++ src/centl_physics_contact_solver.ml
@@ -27,8 +27,8 @@ let deferred_reason_to_string = function
   | Overlap_detected _ -> "overlap_detected"
   | Ambiguous_simultaneous_contacts _ -> "ambiguous_simultaneous_contacts"
 
-let particle_world sphere_world =
-  world (List.map (fun sphere -> sphere.particle) sphere_world.spheres)
+let particle_world state =
+  world (List.map (fun sphere -> sphere.particle) state.spheres)
 
 let vector_exact_equal a b =
   dim_equal a.vector_dimension b.vector_dimension
@@ -48,11 +48,11 @@ let exact_world_conservation before after =
       (world_kinetic_energy before_world)
       (world_kinetic_energy after_world) )
 
-let sphere_by_id sphere_world id =
+let sphere_by_id state id =
   match
     List.find_opt
       (fun sphere -> String.equal sphere.particle.id id)
-      sphere_world.spheres
+      state.spheres
   with
   | Some sphere -> sphere
   | None -> raise (Physics_error ("unknown sphere particle id: " ^ id))
@@ -67,14 +67,14 @@ let touching_count id contacts =
       else count)
     0 contacts
 
-let ambiguous_touching_ids sphere_world contacts =
-  sphere_world.spheres
+let ambiguous_touching_ids state contacts =
+  state.spheres
   |> List.filter (fun sphere -> touching_count sphere.particle.id contacts > 1)
   |> List.map (fun sphere -> sphere.particle.id)
 
-let resolve_touching_pair sphere_world contact =
-  let sphere1 = sphere_by_id sphere_world contact.particle1_id in
-  let sphere2 = sphere_by_id sphere_world contact.particle2_id in
+let resolve_touching_pair state contact =
+  let sphere1 = sphere_by_id state contact.particle1_id in
+  let sphere2 = sphere_by_id state contact.particle2_id in
   let response =
     elastic_collision_3d_at_contact sphere1.particle sphere2.particle
   in
@@ -88,48 +88,46 @@ let resolve_touching_pair sphere_world contact =
       status = response.status;
     } )
 
-let replacement_for id replacements =
-  match List.assoc_opt id replacements with Some particle -> Some particle | None -> None
+let replacement_for id replacements = List.assoc_opt id replacements
 
-let apply_replacements sphere_world replacements =
+let apply_replacements state replacements =
   let spheres =
     List.map
       (fun sphere ->
         match replacement_for sphere.particle.id replacements with
         | Some particle -> { sphere with particle }
         | None -> sphere)
-      sphere_world.spheres
+      state.spheres
   in
   sphere_world spheres
 
-let resolve_isolated_elastic_touching_contacts sphere_world =
-  let contacts = classify_sphere_contacts sphere_world in
+let resolve_isolated_elastic_touching_contacts state =
+  let contacts = classify_sphere_contacts state in
   let overlaps =
     List.filter (fun contact -> contact.relation = Overlapping) contacts
   in
-  if overlaps <> [] then Deferred (sphere_world, Overlap_detected overlaps)
+  if overlaps <> [] then Deferred (state, Overlap_detected overlaps)
   else
     let touching =
       List.filter (fun contact -> contact.relation = Touching) contacts
     in
-    let ambiguous_ids = ambiguous_touching_ids sphere_world touching in
+    let ambiguous_ids = ambiguous_touching_ids state touching in
     if ambiguous_ids <> [] then
-      Deferred
-        (sphere_world, Ambiguous_simultaneous_contacts ambiguous_ids)
+      Deferred (state, Ambiguous_simultaneous_contacts ambiguous_ids)
     else
       let replacements, pair_resolutions =
         List.fold_left
           (fun (replacements, pair_resolutions) contact ->
             let pair_replacements, pair_resolution =
-              resolve_touching_pair sphere_world contact
+              resolve_touching_pair state contact
             in
             ( List.rev_append pair_replacements replacements,
               pair_resolution :: pair_resolutions ))
           ([], []) touching
       in
-      let resolved_world = apply_replacements sphere_world replacements in
+      let resolved_world = apply_replacements state replacements in
       let momentum_conserved, kinetic_energy_conserved =
-        exact_world_conservation sphere_world resolved_world
+        exact_world_conservation state resolved_world
       in
       Completed
         {
